b7cb9e47e69ff386e9c44587556c1ee6f1157342
mchmielarz <Michal.Chmielarz@4finance.com>
2016-08-22 10:33:18 +0200

    Project cleanup.

--- src/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.devthoughts.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-    Book findByTitle(String title);
-
-}

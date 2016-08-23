7492606021dacd2d8efeb4cdf2aebc3311ecc08e
mchmielarz <Michal.Chmielarz@4finance.com>
2016-08-23 18:24:11 +0200

    Not used import removed.

--- src/main/java/pl/devthoughts/spring/web/BookController.java
+++ src/main/java/pl/devthoughts/spring/web/BookController.java
@@ -2,7 +2,6 @@ package pl.devthoughts.spring.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import pl.devthoughts.spring.domain.Book;
